e84183b8a97df6ec3b5866d0245ae72cabf550ba
debbs34 <167197445+debbs34@users.noreply.github.com>
2024-04-23 20:12:10 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+import RedirectAfterDelay from "./script";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <RedirectAfterDelay />
+    </div>
+  );
+}
